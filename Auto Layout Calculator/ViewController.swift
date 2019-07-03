e28a95cecef683535188039dff36a872d86b110f
Alexa Sih <alexa17cassidy@gmail.com>
2019-07-03 10:53:55 -0700

    numberPressed

--- Auto Layout Calculator/ViewController.swift
+++ Auto Layout Calculator/ViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var numberSelected: String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        numberSelected = sender.titleLabel?.text! ?? "error"
+        print(numberSelected)
+    }
 }
-
